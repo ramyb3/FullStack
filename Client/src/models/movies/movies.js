88c2c301fed94c74224dbf1f23aa9f89d055f916
ramy <ramy_3@walla.com>
2023-03-06 19:37:42 +0200

    need to split movies to 2

--- Client/src/models/movies/movies.js
+++ Client/src/models/movies/movies.js
@@ -11,6 +11,7 @@ export default function Movies(props) {
   const [subs, setSubs] = useState([]);
   const [add, setAdd] = useState(false);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
@@ -27,7 +28,7 @@ export default function Movies(props) {
     }
   }, []);
 
-  const edit = async (obj) => {
+  const deleteMovie = async (obj) => {
     await apiCalls("delete", `deleteMovie/${obj}`);
   };
 
@@ -46,141 +47,126 @@ export default function Movies(props) {
   };
 
   return (
-    <div>
-      <h2 style={{ textAlign: "center" }}>Movies Page</h2>
+    <>
+      <div className="flex">
+        <h2>Movies Page</h2>
 
-      {props.data.perm.includes("Create Movies") ? (
-        <div style={{ textAlign: "center" }}>
-          <Button link="" text="All Movies" onClick={() => setAdd(false)} />
-          &nbsp;
-          <Button
-            link="addMovie"
-            text="Add Movie"
-            onClick={() => {
-              setAdd(true);
-              setSearch("");
-            }}
-          />
-          <br />
-          <br />
-        </div>
-      ) : null}
+        {props.data.perm.includes("Create Movies") ? (
+          <div style={{ display: "flex", gap: "10px" }}>
+            <Button link="" text="All Movies" onClick={() => setAdd(false)} />
+            <Button
+              link="addMovie"
+              text="Add Movie"
+              onClick={() => {
+                setAdd(true);
+                setSearch("");
+              }}
+            />
+          </div>
+        ) : null}
+
+        {loading ? <h3>Loading...</h3> : null}
+      </div>
 
       <Outlet />
 
-      {add == false ? (
-        <div style={{ textAlign: "center" }}>
-          {" "}
+      {!add ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            paddingBottom: "10px",
+          }}
+        >
           <input
             placeholder="Find Movie"
-            type="text"
             onChange={(e) => setSearch(e.target.value)}
           />
           <button style={{ fontSize: "14px", height: "22px" }} onClick={find}>
             Find
           </button>
-          <br />
-          <br />
         </div>
       ) : null}
 
-      {add == false
+      {!add
         ? movies.map((item, index) => {
             return (
-              <div key={index}>
-                <div className="box1">
-                  <h2>
-                    {" "}
-                    {item.Name}, {item.Premiered.slice(0, 4)}{" "}
-                  </h2>
-
-                  <big>
-                    <b> Genres: </b>
-                    {item.Genres.map((x, index) => {
-                      return (
-                        <>
-                          {index != item.Genres.length - 1 ? (
-                            <> {x}, </>
-                          ) : (
-                            <> {x} </>
-                          )}
-                        </>
-                      );
-                    })}
-                    <br />
-                    <br />
-                  </big>
-
-                  <img src={item.Image} width="60%" height="60%" />
-                  <br />
-                  <br />
-
+              <div
+                key={index}
+                className="box1 flex"
+                style={{ width: "27em", marginBottom: "10px" }}
+              >
+                <h2>{`${item.Name}, ${item.Premiered.slice(0, 4)}`}</h2>
+                <big style={{ paddingBottom: "10px" }}>
+                  <b>Genres: </b>
+                  {item.Genres.map((genre, index) => {
+                    return `${genre}${
+                      index !== item.Genres.length - 1 ? ", " : ""
+                    }`;
+                  })}
+                </big>
+                <img src={item.Image} width="60%" height="60%" />
+                <div style={{ display: "flex", gap: "10px", padding: "15px" }}>
                   {props.data.perm.includes("Update Movies") ? (
                     <Button link={`editMovie/${item._id}`} text="Edit" />
                   ) : null}
-
                   {props.data.perm.includes("Delete Movies") ? (
                     <Button
                       link=""
                       text="Delete"
-                      onClick={() => edit(item._id)}
+                      onClick={() => deleteMovie(item._id)}
                     />
                   ) : null}
-                  <br />
-                  <br />
-
-                  {props.data.perm.includes("View Subscriptions") ? (
-                    <div className="box2">
-                      <big>
-                        <b>
-                          <Comp data={item} subs={subs} />
-                        </b>
-
-                        {subs.map((i) => {
-                          return i.Movies.map((j) => {
-                            return (
-                              <ul>
-                                {" "}
-                                {j.MovieId == item._id ? (
-                                  <li>
-                                    {members.map((k) => {
-                                      return (
-                                        <div>
-                                          {k._id == i.MemberId ? (
-                                            <div>
-                                              <Link
-                                                to={
-                                                  "/main/subscriptions/" +
-                                                  i.MemberId
-                                                }
-                                              >
-                                                {k.Name}
-                                              </Link>
-                                              &nbsp;,&nbsp;
-                                              {j.Date.slice(8, 10)}/
-                                              {j.Date.slice(5, 7)}/
-                                              {j.Date.slice(0, 4)}
-                                            </div>
-                                          ) : null}
-                                        </div>
-                                      );
-                                    })}
-                                  </li>
-                                ) : null}
-                              </ul>
-                            );
-                          });
-                        })}
-                      </big>
-                    </div>
-                  ) : null}
-                  <br />
                 </div>
-                <br />
+
+                {props.data.perm.includes("View Subscriptions") ? (
+                  <div className="box2" style={{ width: "21em" }}>
+                    <b>
+                      <Comp data={item} subs={subs} />
+                    </b>
+
+                    {subs.map((i) => {
+                      return i.Movies.map((j, index1) => {
+                        return (
+                          <ul>
+                            {j.MovieId === item._id ? (
+                              <li key={index1}>
+                                {members.map((k, index2) => {
+                                  return k._id === i.MemberId ? (
+                                    <div
+                                      key={index2}
+                                      style={{
+                                        display: "flex",
+                                        justifyContent: "space-between",
+                                        marginLeft: "-30px",
+                                        paddingRight: "5px",
+                                      }}
+                                    >
+                                      <Link
+                                        to={`/main/subscriptions/${i.MemberId}`}
+                                      >
+                                        {k.Name}
+                                      </Link>
+                                      <span>
+                                        {j.Date.slice(8, 10)}/
+                                        {j.Date.slice(5, 7)}/
+                                        {j.Date.slice(0, 4)}
+                                      </span>
+                                    </div>
+                                  ) : null;
+                                })}
+                              </li>
+                            ) : null}
+                          </ul>
+                        );
+                      });
+                    })}
+                  </div>
+                ) : null}
               </div>
             );
           })
         : null}
-    </div>
+    </>
   );
 }
